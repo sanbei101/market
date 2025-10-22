6b6672ee5e9b86403ab007033da479a6f39b6648
sanbei101 <sanbei101@outlook.com>
2025-10-22 15:50:34 +0800

    feat: add env support for postgres dsn and reposition logger init

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"market/handle"
 	"market/model"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -12,7 +13,19 @@ import (
 )
 
 func main() {
-	const dsn = "postgres://postgres:secretpassword@101.201.49.155:5432/mydatabase?sslmode=disable"
+	log.DefaultLogger = log.Logger{
+		Level:  log.InfoLevel,
+		Caller: 1,
+		Writer: &log.MultiEntryWriter{
+			&log.ConsoleWriter{ColorOutput: true},
+			// &log.FileWriter{Filename: "app.log", MaxSize: 100 << 20},
+		},
+	}
+	dsn := os.Getenv("POSTGRES_DSN")
+	if dsn == "" {
+		log.Info().Msg("DSN Evnvironment variable not set, using default")
+		dsn = "postgres://postgres:secretpassword@101.201.49.155:5432/mydatabase?sslmode=disable"
+	}
 	config, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to parse DSN")
@@ -23,14 +36,7 @@ func main() {
 	}
 	queries := model.New(pgpool)
 	router := handle.InitRouter(queries)
-	log.DefaultLogger = log.Logger{
-		Level:  log.InfoLevel,
-		Caller: 1,
-		Writer: &log.MultiEntryWriter{
-			&log.ConsoleWriter{ColorOutput: true},
-			// &log.FileWriter{Filename: "app.log", MaxSize: 100 << 20},
-		},
-	}
+
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
